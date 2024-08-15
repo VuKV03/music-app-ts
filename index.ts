ca433f9b02c0a7d867eba2443cc3778ceaeaaddb
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-15 08:32:46 +0700

    build: routes, pug

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("siuu");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+// pug
+app.set("views", "./views");
+app.set("view engine", "pug");
+// End pug
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+})
