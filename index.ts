cf41150e2addd0eb9737e8c200f3e010321117ee
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-19 18:12:29 +0700

    build: dashboard admin

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import clientRoutes from "./routes/client/index.route";
+import adminRoutes from "./routes/admin/index.route";
+
+import path from "path";
 
 // env
 import dotenv from "dotenv";
@@ -8,6 +11,7 @@ dotenv.config();
 
 // database
 import * as database from "./config/database";
+import { systemConfig } from "./config/config";
 database.connect();
 // End database
 
@@ -19,15 +23,28 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 // End pug
 
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+// End App Local Variables
+
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+// End TinyMCE
+
 // static file
 app.use(express.static("public"));
 // End static file
 
 // Client Routes
-
 clientRoutes(app);
 // End Client Routes
 
+// Admin Routes
+adminRoutes(app);
+// End admin Routes
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
