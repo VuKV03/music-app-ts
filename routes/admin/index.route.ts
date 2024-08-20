5694aa732af1def7215244c28803fc5a4f57d770
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-20 07:50:19 +0700

    build: topics admin

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.route";
+import { topicRoutes } from "./topic.route";
 import { systemConfig } from "../../config/config";
 
 const adminRoutes = (app: Express): void => {
@@ -7,6 +8,8 @@ const adminRoutes = (app: Express): void => {
   const path = `/${systemConfig.prefixAdmin}`;
 
   app.use(`${path}/dashboard`, dashboardRoutes);
+
+  app.use(`${path}/topics`, topicRoutes);
 
 };
 
